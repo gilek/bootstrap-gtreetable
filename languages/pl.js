7fdfb423714a529dba35e781944687894473413c
Maciej Klak <klak.maciej@gmail.com>
2014-02-11 11:22:22 +0100

    Initialize

---
+++ languages/pl.js
@@ -0,0 +1,10 @@
+(function( $ ) {
+    $.fn.gtreetable.defaults.languages.pl = {
+        save: 'Zapisz',
+        cancel: 'Anuluj',
+        action: 'Akcja',
+        actionAdd: 'Dodaj',
+        actionEdit: 'Edytuj',
+        actionDelete: 'Usuń'        
+    };
+}( jQuery ));
