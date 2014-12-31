c9f12d7c709b841a4bb388d1107410e09d6e6a8f
= <klak.maciej@gmail.com>
2014-12-31 14:28:25 +0100

    Improved template option.

--- src/js/bootstrap-gtreetable.js
+++ src/js/bootstrap-gtreetable.js
@@ -1,11 +1,3 @@
-/* ========================================================= 
- * bootstrap-gtreetable v2.1.0-alpha
- * https://github.com/gilek/bootstrap-gtreetable
- * ========================================================= 
- * Copyright 2014 Maciej Kłak
- * Licensed under MIT (https://github.com/gilek/bootstrap-gtreetable/blob/master/LICENSE)
- * ========================================================= */
-
 (function ($) {
     // GTREETABLE CLASSES DEFINITION
     // =============================    
@@ -13,72 +5,78 @@
         this.options = options;
         this.$tree = $(element);
         this.language = this.options.languages[this.options.language] === undefined ?
-                this.options.languages.en :
-                $.extend({}, this.options.languages.en, this.options.languages[this.options.language]);
+            this.options.languages['en-US'] :
+            $.extend({}, this.options.languages['en-US'], this.options.languages[this.options.language]);
         this._isNodeDragging = false;
         this._lastId = 0;
+        
+        this.actions = [];
+        if (this.options.defaultActions !== null) {
+            this.actions = this.options.defaultActions;
+        }
+
+        if (this.options.actions !== undefined) {
+            this.actions.push.apply(this.actions, this.options.actions);
+        }        
         
         if (this.options.cache > 0) {
             this.cacheManager = new GTreeTableCache(this);
         }
         
         var lang = this.language;
+        this.template = this.options.template !== undefined ? this.options.template : 
+           '<table class="table gtreetable">' +
+           '<tr class="' + this.options.classes.node + ' ' + this.options.classes.collapsed + '">' +
+           '<td>' +
+           '<span>${draggableIcon}${indent}${ecIcon}${selectedIcon}${typeIcon}${name}</span>' +
+           '<span class="hide ' + this.options.classes.action + '">${input}${saveButton} ${cancelButton}</span>' +
+           '<div class="btn-group pull-right">${actionsButton}${actions}</div>' +
+           '</td>' +
+           '</tr>' +
+           '</table>';            
 
-        if (this.options.template === undefined) {
-            var template = '<table class="table gtreetable">' +
-                '<tr class="' + this.options.classes.node + ' ' + this.options.classes.collapsed + '">' +
-                '<td>' +
-                '<span>';
-
-            if (this.options.draggable === true) {
-                template += '<span class="' + this.options.classes.handleIcon + '">&zwnj;</span>' +
-                    '<span class="' + this.options.classes.draggablePointer + '">&zwnj;</span>';
-            } 
-
-            template += '<span class="' + this.options.classes.indent + '">&zwnj;</span>' +
-                '<span class="' + this.options.classes.ceIcon + ' icon"></span>' +
-                '<span class="' + this.options.classes.selectedIcon + ' icon"></span>' +
-                '<span class="' + this.options.classes.typeIcon + '"></span>' +                
-                '<span class="' + this.options.classes.name + '"></span>' +
-                '</span>' +
-                '<span class="hide ' + this.options.classes.action + '">' +
-                '<input type="text" name="name" value="" style="width: ' + this.options.inputWidth + '" class="form-control" />' +
-                '<button type="button" class="btn btn-sm btn-primary ' + this.options.classes.saveButton + '">' + lang.save + '</button> ' +
-                '<button type="button" class="btn btn-sm ' + this.options.classes.cancelButton + '">' + lang.cancel + '</button>' +
-                '</span>' +
-                '<div class="btn-group pull-right">' +
-                '<button type="button" class="btn btn-sm btn-default dropdown-toggle node-actions" data-toggle="dropdown">' + lang.action + ' <span class="caret"></span></button>' +
-                '<ul class="dropdown-menu" role="menu">' +
-                '<li role="presentation" class="dropdown-header">' + lang.action + '</li>';
-
-            this.actions = [];
-            if (this.options.defaultActions !== null) {
-                this.actions = this.options.defaultActions;
-            }
-
-            if (this.options.actions !== undefined) {
-                this.actions.push.apply(this.actions, this.options.actions);
-            }
+        this.templateParts = this.options.templateParts !== undefined ? this.options.templateParts :
+            {
+                draggableIcon: this.options.draggable === true ? '<span class="' + this.options.classes.handleIcon + '">&zwnj;</span><span class="' + this.options.classes.draggablePointer + '">&zwnj;</span>'  : '',
+                indent: '<span class="' + this.options.classes.indent + '">&zwnj;</span>',
+                ecIcon: '<span class="' + this.options.classes.ceIcon + ' icon"></span>',
+                selectedIcon: '<span class="' + this.options.classes.selectedIcon + ' icon"></span>',
+                typeIcon: '<span class="' + this.options.classes.typeIcon + '"></span>',
+                name: '<span class="' + this.options.classes.name + '"></span>',
+                input: '<input type="text" name="name" value="" style="width: ' + this.options.inputWidth + '" class="form-control" />',
+                saveButton: '<button type="button" class="btn btn-sm btn-primary ' + this.options.classes.saveButton + '">' + lang.save + '</button>',
+                cancelButton: '<button type="button" class="btn btn-sm ' + this.options.classes.cancelButton + '">' + lang.cancel + '</button>',
+                actionsButton: '<button type="button" class="btn btn-sm btn-default dropdown-toggle node-actions" data-toggle="dropdown">' + lang.action + ' <span class="caret"></span></button>',
+                actions: ''
+            };
+            
+        if (this.actions.length > 0) {
+            var templateActions = '<ul class="dropdown-menu" role="menu">' +
+            '<li role="presentation" class="dropdown-header">' + lang.action + '</li>';
 
             $.each(this.actions, function (index, action) {
                 if (action.divider === true) {
-                    template += '<li class="divider"></li>';
+                    templateActions += '<li class="divider"></li>';
                 } 
                 else {
-                    var matches = action.name.match(/\{([\w\W]+)\}/),
+                    var matches = action.name.match(/\$\{([\w\W]+)\}/),
                         name = matches !== null && matches[1] !== undefined && lang.actions[matches[1]] !== undefined ? lang.actions[matches[1]] : action.name;
-                    template += '<li role="presentation"><a href="#notarget" class="node-action-' + index + '" tabindex="-1">' + name + '</a></li>';
+                    templateActions += '<li role="presentation"><a href="#notarget" class="node-action-' + index + '" tabindex="-1">' + name + '</a></li>';
                 }
-            });
+            });        
 
-            template += '</ul>' +
-                    '</div>' +
-                    '</td>' +
-                    '</tr>' +
-                    '</table>';
-
-            this.options.template = template;
+            templateActions += '</ul>';
+            this.templateParts.actions = templateActions;
         }
+        
+        var template = this.template;
+
+        $.each(this.templateParts, function(index, value) {
+            template = template.replace('${'+index+'}', value);
+        });
+        
+        this.options.template = template;
+        
 
         if (this.$tree.find('tbody').length === 0) {
             this.$tree.append('<tbody></tbody>');
@@ -121,13 +119,16 @@
         getSourceNodes: function (nodeId, force) {
             var that = this,
                 oNode = this.getNodeById(nodeId),
-                cached = nodeId > 0 && this.options.cache > 0;
+                cached = (nodeId > 0 && this.options.cache > 0);
                 
             if (cached && force !== true) {
                 var data = this.cacheManager.get(oNode);
-                if (data !== undefined) {
-                    return data;
-                }
+                /*if (data !== undefined) {
+                    return {
+                       [that.options.nodesWrapper]: data 
+                    };
+
+                }*/
             }
             
             var sourceOptions = this.options.source(nodeId);
@@ -137,17 +138,20 @@
                         oNode.isLoading(true);
                     }
                 },
-                success: function (data) {
-                    for (var x = 0; x < data.length; x += 1) {
-                        data[x].parent = nodeId;
-                    }
+                success: function (result) {
+                    if (result[that.options.nodesWrapper] !== undefined) {
+                        data = result[that.options.nodesWrapper];
+                        for (var x = 0; x < data.length; x += 1) {
+                            data[x].parent = nodeId;
+                        }
 
-                    if (typeof that.options.sort === "function") {
-                        data.sort(that.options.sort);
-                    }
+                        if (typeof that.options.sort === "function") {
+                            data.sort(that.options.sort);
+                        }
 
-                    if (cached) {
-                        that.cacheManager.set(oNode, data);
+                        if (cached) {
+                            that.cacheManager.set(oNode, data);
+                        }
                     }
                 },
                 error: function (XMLHttpRequest) {
@@ -166,7 +170,8 @@
         init: function () {
             var that = this;
 
-            this.getSourceNodes(0).done(function (data) {
+            this.getSourceNodes(0).done(function (result) {
+                var data = result[that.options.nodesWrapper];
                 for(var x in data) {
                     var oNewNode = new GTreeTableNode(data[x], that);
                     oNewNode.insertIntegral(oNewNode);
@@ -520,8 +525,8 @@
                         handle: '.'+ that.manager.options.classes.handleIcon,
                         start: function (e) {
                             if (!$.browser.webkit) {
-								$(this).data("bs.gtreetable.gtreetablenode.scrollTop", $(window).scrollTop());
-							}
+                                $(this).data("bs.gtreetable.gtreetablenode.scrollTop", $(window).scrollTop());
+                            }
                         },
                         stop: function (e) {
                             that.manager.isNodeDragging(false);
@@ -1147,6 +1152,7 @@
     $.fn.gtreetable.Constructor = GTreeTable;
 
     $.fn.gtreetable.defaults = {
+        nodesWrapper: 'nodes',
         nodeIndent: 16,
         language: 'en',
         inputWidth: '60%',
@@ -1158,7 +1164,7 @@
         dragCanExpand: false,
         showExpandIconOnEmpty: false,        
         languages: {
-            en: {
+            'en-US': {
                 save: 'Save',
                 cancel: 'Cancel',
                 action: 'Action',
@@ -1180,25 +1186,25 @@
         },
         defaultActions: [
             {
-                name: '{createBefore}',
+                name: '${createBefore}',
                 event: function (oNode, oManager) {
                     oNode.add('before', 'default');
                 }
             },
             {
-                name: '{createAfter}',
+                name: '${createAfter}',
                 event: function (oNode, oManager) {
                     oNode.add('after', 'default');
                 }
             },
             {
-                name: '{createFirstChild}',
+                name: '${createFirstChild}',
                 event: function (oNode, oManager) {
                     oNode.add('firstChild', 'default');
                 }
             },
             {
-                name: '{createLastChild}',
+                name: '${createLastChild}',
                 event: function (oNode, oManager) {
                     oNode.add('lastChild', 'default');
                 }
@@ -1207,13 +1213,13 @@
                 divider: true
             },
             {
-                name: '{update}',
+                name: '${update}',
                 event: function (oNode, oManager) {
                     oNode.makeEditable();
                 }
             },
             {
-                name: '{delete}',
+                name: '${delete}',
                 event: function (oNode, oManager) {
                     if (confirm(oManager.language.messages.onDelete)) {
                         oNode.remove();
